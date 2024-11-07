79faba3ea5de59be0c08935ee2205681cff036ea
lumgashi <lumgashi97@hotmail.com>
2024-11-07 16:25:06 +0100

    remove seed func

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { createAssistant } from './utils/functions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await createAssistant();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
